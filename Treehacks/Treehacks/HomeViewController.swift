bd6ac2bdbb10bc3341c787c87da17a5834242a23
Gera Shabanets <gerashabanets@MacBook-Pro-160.local>
2019-02-17 08:09:54 -0800

    init

--- Treehacks/Treehacks/HomeViewController.swift
+++ Treehacks/Treehacks/HomeViewController.swift
@@ -1,7 +1,8 @@
 //
 //  SecondViewController.swift
 //  Treehacks
-//
+
+//  Create by @gerashabanets.
 //  Created by Katie Mishra on 2/16/19.
 //  Copyright © 2019 Katie Mishra. All rights reserved.
 //
@@ -11,6 +12,17 @@ import Firebase
 import FirebaseAuth
 import FirebaseCore
 import FirebaseDatabase
+
+//extension for spacing
+extension UILabel {
+    func addCharacterSpacing(kernValue: Double = 1.45) {
+        if let labelText = text, labelText.count > 0 {
+            let attributedString = NSMutableAttributedString(string: labelText)
+            attributedString.addAttribute(NSAttributedString.Key.kern, value: kernValue, range: NSRange(location: 0, length: attributedString.length - 1))
+            attributedText = attributedString
+        }
+    }
+}
 
 class HomeViewController: UIViewController, UIImagePickerControllerDelegate,
 UINavigationControllerDelegate {
@@ -24,37 +36,53 @@ UINavigationControllerDelegate {
     var currDist: Float = 0.0
     var loaded = false
 
-    @IBOutlet weak var navigationBar: UINavigationBar!
+    //@IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var profilePic: UIImageView!
+    
+    //Purple headers
+    @IBOutlet weak var nameHeader: UILabel!
+    @IBOutlet weak var phoneNumberHeader: UILabel!
+    @IBOutlet weak var progressHeader: UILabel!
+    
     
     @IBOutlet weak var nameVal: UILabel!
     @IBOutlet weak var phoneNum: UILabel!
     @IBOutlet weak var currlvl: UILabel!
     @IBOutlet weak var nextlvl: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var coinLabel: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //ajdusting spacing for purple headers
+        nameHeader.addCharacterSpacing()
+        phoneNumberHeader.addCharacterSpacing()
+        progressHeader.addCharacterSpacing()
+        
+
         //RETRIEVING DATA AND SETTING LABELS
         let ref: DatabaseReference = Database.database().reference()
         let userID = Auth.auth().currentUser?.uid //BUG!
+
         ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
             // Get user value
             let value = snapshot.value as? NSDictionary
             
             self.nameVal.text = "\(value?["firstName"] as? String ?? "") \(value?["lastName"] as? String ?? "")"
+            self.nameVal.addCharacterSpacing()
+            
             self.phoneNum.text = value?["parentsNumber"] as? String ?? ""
-            self.currLevel = value?["Level"] as? Int ?? 1
-            self.currlvl.text = "\(self.currLevel)"
-            self.nextlvl.text = "\(self.currLevel + 1)"
-            self.coinNumber = value?["numCoins"] as? Int ?? 1
-            // ...
+            self.phoneNum.addCharacterSpacing()
+            
         }) { (error) in
             print(error.localizedDescription)
         }
-       
         //PROGRESS BAR
-        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 3)
+       // self.progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 2)
         
         //IMAGE SELECTION
         //***implement
@@ -62,28 +90,50 @@ UINavigationControllerDelegate {
         
     }
     
-    @IBOutlet weak var progressBar: UIProgressView!
-    
     //ADVANCE LENGTH OF BAR
     func advance(_ sender: Bool) {
         currDist = 0
         progressBar.setProgress(currDist, animated: false)
         perform(#selector(updateProgress), with: nil, afterDelay: 0.1)
+
+        currDist = currDist + 0.01
+        perform(#selector(updateProgress), with: nil, afterDelay: 0.001)
     }
     
     //ADVANCE LENGTH OF BAR
     @objc func updateProgress() {
         currDist = currDist + 0.01
-        //print("Curr dist: \(currDist)")
         progressBar.progress = currDist/MAXDIST
         
-        if currDist < (Float)(coinNumber) {
+        if currDist < (Float)(coinNumber % 100) {
             perform(#selector(updateProgress), with: nil, afterDelay: 0.0001)
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
+        
+        let ref: DatabaseReference = Database.database().reference()
+        let userID = Auth.auth().currentUser?.uid
+        
+        ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            
+            //self.currLevel = value?["Level"] as? Int ?? 1
+            self.coinNumber = value?["numCoins"] as? Int ?? 0
+            self.currlvl.text = "Level \(self.coinNumber / 100 + 1)"
+            self.currlvl.addCharacterSpacing()
+            self.nextlvl.text = "Level \(self.coinNumber / 100 + 2)"
+            self.nextlvl.addCharacterSpacing()
+            self.coinLabel.text = "\(self.coinNumber)"
+            self.coinLabel.addCharacterSpacing()
+            //self.coinNumber = 75
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
         loaded = true
         advance(loaded)
     }
--- Treehacks/Treehacks/LoginViewController.swift
+++ Treehacks/Treehacks/LoginViewController.swift
@@ -9,16 +9,22 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var signinButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginButton.layer.cornerRadius = 5
+        self.loginButton.layer.cornerRadius = 15
+        self.signinButton.layer.cornerRadius = 15
+
+        print("signInViewDidLoad")
+        
+        //loginAction(<#T##sender: AnyObject##AnyObject#>)
         // Do any additional setup after loading the view.
     }
     
     //Login Action
     @IBAction func loginAction(_ sender: AnyObject) {
-        
+        //print("loginAction")
         if self.emailTextField.text == "" || self.passwordTextField.text == "" {
             
             //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
@@ -31,20 +37,23 @@ class LoginViewController: UIViewController {
             self.present(alertController, animated: true, completion: nil)
             
         } else {
-            
+            print("Before sign in is called")
             Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
-                
+                //print("inside")
                 if error == nil {
                     
                     //Print into the console if successfully logged in
                     print("You have successfully logged in")
+                    //print("Auth.auth().currentUser?.email")
                     
                     //Go to the HomeViewController if the login is sucessful
                     let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
                     self.present(vc!, animated: true, completion: nil)
                     
+                    
                 } else {
                     
+                    print("Error: ")
                     //Tells the user that there is an error and then gets firebase to tell them the error
                     let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
                     
